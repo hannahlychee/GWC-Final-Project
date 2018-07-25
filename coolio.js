13ce3ab68d16026872361617c7f490e4277339d4
hannahlychee <41088769+hannahlychee@users.noreply.github.com>
2018-07-25 15:15:38 -0700

    Homepage/Hannah

---
+++ coolio.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
